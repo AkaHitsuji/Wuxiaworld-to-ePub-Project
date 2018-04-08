e706747fd0293b7c70525b4ad1f8fd01a2b77820
AkaHitsuji <yang025@e.ntu.edu.sg>
2018-04-08 01:33:59 -0700

    added function to download chapter and function to clean chapter

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Wuxiaworld to ePub Project
 I am a big fan of many wuxiaworld novels and I needed to learn python. But I often get frustrated that i'm not able to save the chapters for offline reading. So I used this as a  motivation to create a python script that downloads chapters and saves them into an ePub format for easy reading on my iPhone.
 
-## Getting Started
+## Prerequisites
 
 You will need to have python 3 installed.
 
-### Step-by-step Tutorial
+## Step-by-step Tutorial
 
 This python script was written and tested on MacOSX. To run first run the following line on terminal.
 
--- main.py
+++ main.py
@@ -1,24 +1,59 @@
 # urllib2 and BeautifulSoup libraries
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
+import requests
+import os
 
 # use Mozilla to open webpage to bypass mod_security that blocks bots and pass into htmltext
-req = Request('https://www.wuxiaworld.com/novel/against-the-gods/atg-chapter-1', headers={'User-Agent': 'Mozilla/5.0'})
+url = 'https://www.wuxiaworld.com/novel/against-the-gods/atg-chapter-1'
+req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 htmltext = urlopen(req).read()
 
 # parse html into BeautifulSoup and store in variable soup
 soup = BeautifulSoup(htmltext, 'html.parser')
 
 # extract story section into ResultSet
-soup_story = soup.find_all("p")
-print(soup_story)
-for line in soup_story:
-    print(line.text)
+# soup_story = soup.find_all("p")
+# print(soup_story)
+# for line in soup_story:
+#     print(line.text)
 
-# extract title
-# chapter_title = soup_story.find("strong")
-# print(chapter_title)
+def download_chapter(url_link, file_name):
+    page = requests.get(url_link).text
+    file = open(file_name, "w", encoding="utf8")
+    file.write(page)
+    file.close()
 
-# to see xml
-# print(soup.prettify())
-# print(soup.get_text())
+def clean_chapter(file_in, file_out):
+    raw = open(file_in, "r", encoding="utf8")
+    soup = BeautifulSoup(raw, 'html.parser')
+    soup = soup.find("div", {"class": 'fr-view'})
+    text = soup.get_text(separator='\n')
+    text = text.replace("Previous Chapter", "").replace("Next Chapter", "")
+    text = text.lstrip().rstrip()
+    # find chapter title, store and remove it
+    chapter_title = text.split('\n', 1)[0]
+    text = text.replace(chapter_title, "")
+    text = text.lstrip().rstrip()
+    raw.close()
+    file = open(file_out, "w", encoding="utf8")
+    file.write(text)
+    file.close()
+    os.remove(file_in)
+
+download_chapter(url, 'test.html')
+clean_chapter('test.html','clean.html')
+
+
+
+
+# create epub
+# epub = zipfile.ZipFile(novelname + "_" + chapter_s + "-" + chapter_e + ".epub", "w")
+# epub.writestr("mimetype", "application/epub+zip")
+# # creating the container.xml file
+# epub.writestr("META-INF/container.xml", '''<container version="1.0"
+# xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
+#   <rootfiles>
+#     <rootfile full-path="OEBPS/Content.opf" media-type="application/oebps-package+xml"/>
+#   </rootfiles>
+# </container>''')
