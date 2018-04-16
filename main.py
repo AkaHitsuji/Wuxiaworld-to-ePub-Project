705165fcfdce6643b9d141886da9c9a7e586c789
AkaHitsuji <yang025@e.ntu.edu.sg>
2018-04-16 15:57:59 -0700

    Added download UI to tkinter

--- main.py
+++ main.py
@@ -14,6 +14,23 @@ list_of_novels = []
 for title in novels:
     list_of_novels.append(title)
 
+# button clicked function
+def clicked():
+    selected_novel = selected_option.get()
+    lbl_download.configure(text="Button clicked! "+selected_novel+" selected.")
+
+    novel_name = novels[selected_novel]
+
+    list_of_links = functions.download_links(novel_url+novel_name, novel_name)
+    lbl_numOfChapters.configure(text="Number of Chapters: " + str(len(list_of_links)))
+    print("Number of Chapters: ", len(list_of_links))
+
+    functions.download_cover(chinese_novel_url, novel_name)
+
+    functions.create_epub(selected_novel, list_of_links)
+    lbl_confirmation.configure(text="ePub created")
+    print("ePub created")
+
 # tkinter code
 window = Tk()
 
@@ -29,28 +46,26 @@ selected_option["values"] = list_of_novels
 selected_option.grid(column = 0, row = 1)
 selected_option.current(0)
 
-def clicked():
-    lbl.configure(text="Button was clicked !!")
-
 btn = Button(window, text="Click Me", command=clicked)
 btn.grid(column=0, row=2)
 
+lbl_download = Label(window, text="")
+lbl_download.grid(column=0, row=3)
+
+lbl_numOfChapters = Label(window, text="")
+lbl_numOfChapters.grid(column=0, row=4)
+
+lbl_confirmation = Label(window, text="")
+lbl_confirmation.grid(column=0, row=5)
+
 window.mainloop()
 
-# main code
-while True:
-    selected_novel = input("Name of novel: ")
-    if selected_novel in novels:
-        break
-    else:
-        print("Novel does not exist, please input again.")
-
-novel_name = novels[selected_novel]
-
-list_of_links = functions.download_links(novel_url+novel_name, novel_name)
-print("Number of Chapters: ", len(list_of_links))
-
-functions.download_cover(chinese_novel_url, novel_name)
-
-functions.create_epub(selected_novel, list_of_links)
-print("ePub created")
+# # main code
+# while True:
+#     selected_novel = input("Name of novel: ")
+#     if selected_novel in novels:
+#         break
+#     else:
+#         print("Novel does not exist, please input again.")
+#
+# novel_name = novels[selected_novel]
